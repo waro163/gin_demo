b2391a6f5cd0d18fadcb366ddcb6e1afcaf50d4d
waro163 <waro163@163.com>
2022-08-09 00:02:40 +0800

    fix bug

--- app/download/view.go
+++ app/download/view.go
@@ -5,7 +5,9 @@ import (
 )
 
 func DownloadDemo(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"name": "",
-	})
+	// ctx.JSON(200, gin.H{
+	// 	"hi": "world",
+	// })
+	ctx.FileAttachment("/Users/waro/work/gin_demo/main.go", "main.go")
+
 }
