13e7a0ca0b4027cf1eda1fbaa1d093037c16a79c
waro163 <waro163@163.com>
2022-08-10 23:57:24 +0800

    add upload file proxy

--- app/upload/views.go
+++ app/upload/views.go
@@ -21,3 +21,21 @@ func UploadDemo(ctx *gin.Context) {
 		"message": "ok",
 	})
 }
+
+func UploadToThird(ctx *gin.Context) {
+	url := "http://127.0.0.1:8081/upload/multi"
+	req, err := http.NewRequest(ctx.Request.Method, url, ctx.Request.Body)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+		return
+	}
+	req.Header = ctx.Request.Header
+	transport := http.DefaultTransport
+	res, err := transport.RoundTrip(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+		return
+	}
+	// ctx.Data(res.StatusCode, res.Header.Get("Content-Type"), res.Body)
+	ctx.DataFromReader(res.StatusCode, res.ContentLength, res.Header.Get("Content-Type"), res.Body, nil)
+}
