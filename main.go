5e5ca011e9da10caefe8496351ae4f0baa412d29
Wang Rong <wangron@tesla.com>
2022-09-14 11:31:55 +0800

    add otel to tracing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"gin_demo/app/cookie"
@@ -9,22 +10,40 @@ import (
 	"gin_demo/app/proxy"
 	"gin_demo/app/upload"
 	_ "gin_demo/config"
+	"log"
 
 	// _ "gin_demo/log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 )
 
-var port uint64
+var (
+	port       uint64
+	servername string
+)
 
 func init() {
-	flag.Uint64Var(&port, "Port", 8080, "web server listen port")
+	flag.Uint64Var(&port, "port", 8080, "web server listen port")
+	flag.StringVar(&servername, "name", "gin_demo", "web server name")
 	flag.Parse()
 }
 
 func main() {
+
+	tp := initOteleMetry(servername)
+	defer tp.Shutdown(context.Background())
+
 	router := gin.Default()
+	router.Use(otelgin.Middleware("gin_demo"))
 
 	router.GET("/ping", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "pong")
@@ -45,4 +64,40 @@ func main() {
 	proxy.RegisterRouter(proxyApp)
 
 	router.Run(fmt.Sprintf(":%d", port))
+}
+
+func initOteleMetry(name string) *tracesdk.TracerProvider {
+	tp, err := tracerProvider("http://localhost:14268/api/traces", name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp
+}
+
+func tracerProvider(url, name string) (*tracesdk.TracerProvider, error) {
+	// Create the Jaeger exporter
+	// using http directly send
+	// exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+
+	// using udp send agend
+	exp, err := jaeger.New(jaeger.WithAgentEndpoint())
+	if err != nil {
+		return nil, err
+	}
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// tracesdk.WithSampler(tracesdk.AlwaysSample()),
+		tracesdk.WithSampler(tracesdk.ParentBased(tracesdk.TraceIDRatioBased(0.5))),
+		// Record information about this application in a Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(name),
+			attribute.String("environment", "dev"),
+			// attribute.Int64("ID", id),
+		)),
+	)
+	return tp, nil
 }
